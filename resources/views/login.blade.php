ecce99e832a758bf89de465aba225fd178fe03e0
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-18 17:18:25 +0100

    added form to login page

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -2,14 +2,34 @@
     'show_header' => false,
 ])
 
-@section('body_classes', 'page-login full-height')
+@section('body_classes', 'page-login full-height grey lighten-4')
 
 @section('content')
     <div class="valign-wrapper full-height">
         <div class="container">
         <div class="row">
             <div class="col s4 offset-s4">
-                Login
+
+                <div class="card">
+                    <div class="card-content">
+                        <span class="card-title">Login</span>
+
+                        <form id="login-form" name="login-form">
+                            <div class="input-field">
+                                <input id="email" type="email" autocomplete="off" class="validate">
+                                <label for="email">Email</label>
+                            </div>
+                            <div class="input-field">
+                                <input id="password" type="password" class="validate">
+                                <label for="password">Password</label>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="card-action">
+                        <button class="waves-effect waves-light btn">Login</button>
+                    </div>
+                </div>
+
             </div>
         </div>
     </div>
