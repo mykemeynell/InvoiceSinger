cc90660be33bba3bf6de01ad2bf8eee7f9e2ec6d
Myke Meynell <myke@wearebump.com>
2019-06-20 23:41:58 +0100

    modal for creating new invoices

--- resources/views/invoices.blade.php
+++ resources/views/invoices.blade.php
@@ -35,7 +35,37 @@
 @endsection
 
 @push('nav-extra')
-    <a class="btn-floating btn-large halfway-fab waves-effect waves-light">
+    <a class="btn-floating btn-large halfway-fab waves-effect waves-light modal-trigger" href="#new-invoice-modal">
         <i class="material-icons">add</i>
     </a>
+@endpush
+
+@push('end')
+    <div id="new-invoice-modal" class="modal">
+        <div class="modal-content">
+            <h4>Create New Invoice</h4>
+            <form name="new-invoice-form" id="new-invoice-form">
+                <div class="row">
+                    <div class="col s12 input-field">
+                        <select name="invoice[client_id]">
+                            <option value="" disabled selected>Choose your option</option>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                            <option value="3">Option 3</option>
+                        </select>
+                        <label>Materialize Select</label>
+                    </div>
+                </div>
+            </form>
+        </div>
+        <div class="modal-footer">
+            <a href="#!" class="modal-close waves-effect waves-green btn-flat">Agree</a>
+        </div>
+    </div>
+
+    <script>
+        $(document).ready(function(){
+            $('.modal').modal();
+        });
+    </script>
 @endpush
