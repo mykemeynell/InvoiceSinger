d5854d6a5341fb9f448d8c327f25ab5c368222ad
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-20 16:52:00 +0100

    Created Invoice Repository

---
+++ app/Storage/Repository/Contract/InvoiceRepositoryInterface.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace InvoiceSinger\Storage\Repository\Contract;
+
+use ArchLayer\Repository\RepositoryInterface;
+
+/**
+ * Interface InvoiceRepositoryInterface.
+ *
+ * @package InvoiceSinger\Storage\Repository\Contract
+ */
+interface InvoiceRepositoryInterface extends RepositoryInterface
+{
+    //
+}
---
+++ app/Storage/Repository/InvoiceRepository.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace InvoiceSinger\Storage\Repository;
+
+use ArchLayer\Repository\Repository;
+use InvoiceSinger\Storage\Repository\Contract\InvoiceRepositoryInterface;
+
+/**
+ * Class InvoiceRepository.
+ *
+ * @package InvoiceSinger\Storage\Repository
+ */
+class InvoiceRepository extends Repository implements InvoiceRepositoryInterface
+{
+    //
+}
