cfe2655c934d44ef2ab3f760b4d2f84cae0dd958
Myke Meynell <myke@wearebump.com>
2019-06-20 23:47:38 +0100

    Pulling invoices into controller

--- app/Http/Controllers/Invoices/InvoiceController.php
+++ app/Http/Controllers/Invoices/InvoiceController.php
@@ -2,24 +2,43 @@
 
 namespace InvoiceSinger\Http\Controllers\Invoices;
 
+use ArchLayer\Service\Contract\ServiceInterface;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 use InvoiceSinger\Http\Controllers\Controller;
+use InvoiceSinger\Storage\Service\Contract\InvoiceServiceInterface;
+use InvoiceSinger\Support\Concern\HasService;
 
 /**
  * Class InvoiceController
+ *
+ * @method InvoiceServiceInterface getService(?string $name = null) : ServiceInterface
  *
  * @package InvoiceSinger\Http\Controllers\Invoices
  */
 class InvoiceController extends Controller
 {
+    use HasService;
+
+    /**
+     * InvoiceController constructor.
+     *
+     * @param \InvoiceSinger\Storage\Service\Contract\InvoiceServiceInterface $service
+     */
+    function __construct(InvoiceServiceInterface $service)
+    {
+        $this->setService($service);
+    }
+
     /**
      * Get the list of invoices.
      *
      * @return \Illuminate\View\View
+     * @throws \Exception
      */
     public function index(): View
     {
-        return view('invoices');
+        return view('invoices')
+            ->with('invoices', $this->getService()->fetch());
     }
 }
