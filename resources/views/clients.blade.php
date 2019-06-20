5ee31762dfdf40dbbcf2d6cb2ede3b3b075f65a9
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-20 11:43:07 +0100

    removed modal trigger from view

--- resources/views/clients.blade.php
+++ resources/views/clients.blade.php
@@ -33,7 +33,7 @@
 @endsection
 
 @push('nav-extra')
-    <a class="btn-floating btn-large halfway-fab waves-effect waves-light modal-trigger">
+    <a class="btn-floating btn-large halfway-fab waves-effect waves-light">
         <i class="material-icons">add</i>
     </a>
 @endpush
