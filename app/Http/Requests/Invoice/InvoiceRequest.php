3cd4718103c54d5dccd99a8480875fcca6ba4546
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-21 18:52:06 +0100

    added invoice request

---
+++ app/Http/Requests/Invoice/InvoiceRequest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace InvoiceSinger\Http\Requests\Invoice;
+
+use Illuminate\Foundation\Http\FormRequest;
+use InvoiceSinger\Requests\Concern\RequestHasNoRules;
+use InvoiceSinger\Requests\Concern\RequestHasParameterBag;
+use InvoiceSinger\Requests\Concern\RequestIsAuthorized;
+use InvoiceSinger\Storage\Entity\Contract\InvoiceEntityInterface;
+use InvoiceSinger\Support\Concern\HasService;
+
+/**
+ * Class InvoiceRequest.
+ *
+ * @package InvoiceSinger\Http\Requests\Invoice
+ */
+class InvoiceRequest extends FormRequest
+{
+    use RequestIsAuthorized, RequestHasNoRules, RequestHasParameterBag, HasService;
+
+    /**
+     * If an invoice ID is contained within the request, this method will attempt to find and
+     * return the associated invoice entity.
+     *
+     * @return \InvoiceSinger\Storage\Entity\Contract\InvoiceEntityInterface|null
+     */
+    public function client(): ?InvoiceEntityInterface
+    {
+        if($id = $this->getParameterBag()->get('id')) {
+            return app('invoice.service')->find($id);
+        }
+
+        if($id = $this->route('invoice_id')) {
+            return app('invoice.service')->find($id);
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the payload namespace.
+     *
+     * @return string
+     */
+    protected function getPayloadNamespace(): string
+    {
+        return 'invoice';
+    }
+}
