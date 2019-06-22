38c35d5049c4ccb75ef9ea02a3b725c7ec6df018
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-22 16:29:18 +0100

    added family name migration

---
+++ database/migrations/2019_06_22_152727_create_product_families_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use UuidColumn\Concern\UuidColumn;
+
+/**
+ * Class CreateProductFamiliesTable.
+ */
+class CreateProductFamiliesTable extends Migration
+{
+    use UuidColumn;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_families', function (Blueprint $table) {
+            $this->createUuidColumn($table, 'id');
+
+            $table->string('name');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_families');
+    }
+}
