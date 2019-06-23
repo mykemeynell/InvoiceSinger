7d0367cab544a98209f1ad879e5e35738aee377c
Myke Meynell <myke@wearebump.com>
2019-06-23 23:09:16 +0100

    added migration for tax rates table

---
+++ database/migrations/2019_06_23_220749_create_tax_rates_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use UuidColumn\Concern\UuidColumn;
+
+/**
+ * Class CreateTaxRatesTable
+ */
+class CreateTaxRatesTable extends Migration
+{
+    use UuidColumn;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tax_rates', function (Blueprint $table) {
+            $this->createUuidColumn($table, 'id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tax_rates');
+    }
+}
