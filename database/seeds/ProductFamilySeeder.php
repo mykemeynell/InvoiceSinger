5bfba87af3c3e0325f3e6d4322131c71e8f66310
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-24 00:23:00 +0100

    added seeder for default product family"

---
+++ database/seeds/ProductFamilySeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+/**
+ * Class ProductFamilySeeder.
+ */
+class ProductFamilySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // TODO: Create "Default Product Family"
+    }
+}
