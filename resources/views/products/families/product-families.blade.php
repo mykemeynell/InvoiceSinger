a7de24f3f0c695c1b8f0266552b5ed6cfac74355
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-25 15:30:30 +0100

    added FAB and container top spacing

--- resources/views/products/families/product-families.blade.php
+++ resources/views/products/families/product-families.blade.php
@@ -1,19 +1,13 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container margin-top-30">
         <div class="row">
             <div class="col s12">
                 <table class="responsive-table">
                     <thead>
                     <tr>
-                        <th>Payment Date</th>
-                        <th>Invoice Date</th>
-                        <th>Invoice</th>
-                        <th>Client</th>
-                        <th class="right-align">Amount</th>
-                        <th>Payment Method</th>
-                        <th>Note</th>
+                        <th>Family Name</th>
                         <th class="right-align">Options</th>
                     </tr>
                     </thead>
@@ -21,12 +15,6 @@
                     <tbody>
                     <tr>
                         <td>18/06/2019</td>
-                        <td>12/06/2019</td>
-                        <td><a href="#">INV-18-0032</a></td>
-                        <td><a href="#">Silicon Labs</a></td>
-                        <td class="right-align">&pound;0.00</td>
-                        <td>Online Payment</td>
-                        <td>Paid via WorldPay integration.</td>
                         <td class="right-align"><a class="waves-effect waves-light btn">View</a></td>
                     </tr>
                     </tbody>
@@ -35,3 +23,9 @@
         </div>
     </div>
 @endsection
+
+@push('nav-extra')
+    <a class="btn-floating btn-large halfway-fab waves-effect waves-light">
+        <i class="material-icons">add</i>
+    </a>
+@endpush
