f413f8b16f11ab381ed0d7763710fbd2aebd5945
Myke Meynell <myke.meynell@rarecreativegroup.com>
2019-06-25 13:57:05 +0100

    added methods for managing tax rates

--- app/Storage/Service/Contract/TaxRateServiceInterface.php
+++ app/Storage/Service/Contract/TaxRateServiceInterface.php
@@ -3,6 +3,9 @@
 namespace InvoiceSinger\Storage\Service\Contract;
 
 use ArchLayer\Service\Contract\ServiceInterface;
+use Illuminate\Database\Eloquent\Collection;
+use InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * Interface TaxRateServiceInterface.
@@ -11,4 +14,47 @@ use ArchLayer\Service\Contract\ServiceInterface;
  */
 interface TaxRateServiceInterface extends ServiceInterface
 {
+    /**
+     * Fetch all tax rates from the database.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function fetch(): Collection;
+
+    /**
+     * Attempt to find a tax rate entity in the database using its ID.
+     *
+     * @param string $id
+     *
+     * @return \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function findUsingId(string $id): ?TaxRateEntityInterface;
+
+    /**
+     * Create a new tax rate entity and store it in the database.
+     *
+     * @param \Symfony\Component\HttpFoundation\ParameterBag $payload
+     *
+     * @return \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function create(ParameterBag $payload): ?TaxRateEntityInterface;
+
+    /**
+     * Update an existing tax rate entity and save the changes to the database.
+     *
+     * @param \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model $entity
+     * @param \Symfony\Component\HttpFoundation\ParameterBag                $payload
+     *
+     * @return bool
+     */
+    public function update(TaxRateEntityInterface $entity, ParameterBag $payload): bool;
+
+    /**
+     * Delete an existing tax rate entity.
+     *
+     * @param \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model $entity
+     *
+     * @return bool
+     */
+    public function delete(TaxRateEntityInterface $entity): bool;
 }
--- app/Storage/Service/TaxRateService.php
+++ app/Storage/Service/TaxRateService.php
@@ -3,8 +3,12 @@
 namespace InvoiceSinger\Storage\Service;
 
 use ArchLayer\Service\Service;
+use Illuminate\Database\Eloquent\Collection;
+use Illuminate\Support\Arr;
+use InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface;
 use InvoiceSinger\Storage\Repository\Contract\TaxRateRepositoryInterface;
 use InvoiceSinger\Storage\Service\Contract\TaxRateServiceInterface;
+use Symfony\Component\HttpFoundation\ParameterBag;
 
 /**
  * Class TaxRateService.
@@ -21,5 +25,73 @@ class TaxRateService extends Service implements TaxRateServiceInterface
     function __construct(TaxRateRepositoryInterface $repository)
     {
         $this->setRepository($repository);
+    }
+
+    /**
+     * Fetch all tax rates from the database.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function fetch(): Collection
+    {
+        return $this->getRepository()->builder()->get();
+    }
+
+    /**
+     * Attempt to find a tax rate entity in the database using its ID.
+     *
+     * @param string $id
+     *
+     * @return \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function findUsingId(string $id): ?TaxRateEntityInterface
+    {
+        return $this->getRepository()->findUsingId($id);
+    }
+
+    /**
+     * Create a new tax rate entity and store it in the database.
+     *
+     * @param \Symfony\Component\HttpFoundation\ParameterBag $payload
+     *
+     * @return \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model|null
+     */
+    public function create(ParameterBag $payload): ?TaxRateEntityInterface
+    {
+        /** @var \InvoiceSinger\Storage\Entity\TaxRateEntity $entity */
+        $entity = $this->getRepository()->create(
+            Arr::only($payload->all(), $this->getRepository()->getModel()->getFillable())
+        );
+        $entity->save();
+
+        return $entity;
+    }
+
+    /**
+     * Update an existing tax rate entity and save the changes to the database.
+     *
+     * @param \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model $entity
+     * @param \Symfony\Component\HttpFoundation\ParameterBag                                                    $payload
+     *
+     * @return bool
+     */
+    public function update(TaxRateEntityInterface $entity, ParameterBag $payload): bool
+    {
+        return $entity->update(
+            Arr::only($payload->all(), $this->getRepository()->getModel()->getFillable())
+        );
+    }
+
+    /**
+     * Delete an existing tax rate entity.
+     *
+     * @param \InvoiceSinger\Storage\Entity\Contract\TaxRateEntityInterface|\Illuminate\Database\Eloquent\Model $entity
+     *
+     * @return bool
+     * @throws \Exception
+     */
+    public function delete(TaxRateEntityInterface $entity): bool
+    {
+        return $entity->delete();
     }
 }
