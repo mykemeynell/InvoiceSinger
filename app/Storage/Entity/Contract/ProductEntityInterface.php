75e5a3efd429d3b202a0dea67cd05e2a9ef43984
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-26 16:52:39 +0100

    added product entity

---
+++ app/Storage/Entity/Contract/ProductEntityInterface.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace InvoiceSinger\Storage\Entity\Contract;
+
+/**
+ * Interface ProductEntityInterface.
+ *
+ * @property string              $name
+ * @property string|null         $sku
+ * @property string|null         $description
+ * @property string              $family
+ * @property string              $unit
+ * @property string              $price
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ *
+ * @package InvoiceSinger\Storage\Entity\Contract
+ */
+interface ProductEntityInterface
+{
+    /**
+     * Get the display name.
+     *
+     * @return string
+     */
+    public function getDisplayName(): string;
+
+    /**
+     * Get the SKU.
+     *
+     * @return string|null
+     */
+    public function getSku(): ?string;
+
+    /**
+     * Get the product family.
+     *
+     * @return string|null
+     */
+    public function getDescription(): ?string;
+
+    /**
+     * Get the product family.
+     *
+     * @return string
+     */
+    public function getFamily(): string;
+
+    /**
+     * Get the unit.
+     *
+     * @return string
+     */
+    public function getUnit(): string;
+
+    /**
+     * Get the product price.
+     *
+     * @return float
+     */
+    public function getPrice(): float;
+}
---
+++ app/Storage/Entity/ProductEntity.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace InvoiceSinger\Storage\Entity;
+
+use ArchLayer\Entity\Concern\EntityHasTimestamps;
+use Illuminate\Database\Eloquent\Model;
+use InvoiceSinger\Storage\Entity\Contract\ProductEntityInterface;
+use UuidColumn\Concern\HasUuidObserver;
+
+/**
+ * Class ProductEntity.
+ *
+ * @package InvoiceSinger\Storage\Entity
+ */
+class ProductEntity extends Model implements ProductEntityInterface
+{
+    use HasUuidObserver, EntityHasTimestamps;
+
+    /**
+     * The table name.
+     *
+     * @var string
+     */
+    public $table = 'products';
+
+    /**
+     * Indicates if the IDs are auto-incrementing.
+     *
+     * @var bool
+     */
+    public $incrementing = false;
+
+    /**
+     * The "type" of the primary key ID.
+     *
+     * @var string
+     */
+    protected $keyType = 'string';
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'created_at',
+        'updated_at',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'price' => 'float(10,2)',
+    ];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'sku',
+        'description',
+        'family',
+        'unit',
+        'price',
+    ];
+
+    /**
+     * Get the display name.
+     *
+     * @return string
+     */
+    public function getDisplayName(): string
+    {
+        return ucwords($this->name);
+    }
+
+    /**
+     * Get the SKU.
+     *
+     * @return string|null
+     */
+    public function getSku(): ?string
+    {
+        return $this->sku;
+    }
+
+    /**
+     * Get the product family.
+     *
+     * @return string|null
+     */
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    /**
+     * Get the product family.
+     *
+     * @return string
+     */
+    public function getFamily(): string
+    {
+        return $this->family;
+    }
+
+    /**
+     * Get the unit.
+     *
+     * @return string
+     */
+    public function getUnit(): string
+    {
+        return $this->unit;
+    }
+
+    /**
+     * Get the product price.
+     *
+     * @return float
+     */
+    public function getPrice(): float
+    {
+        return $this->price;
+    }
+}
