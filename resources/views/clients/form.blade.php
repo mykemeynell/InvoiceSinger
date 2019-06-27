ba20f81d9650cae88737c1b7efb97635e5a777c3
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-27 18:40:01 +0100

    fixed delete client button

--- resources/views/clients/form.blade.php
+++ resources/views/clients/form.blade.php
@@ -6,8 +6,6 @@
 
     {{-- End form action buttons --}}
 
-    <form id="client-form" name="client-form">
-        {!! csrf_field() !!}
         <div class="container">
             <div class="row margin-y-30">
                 <div class="col s12 m6">
@@ -19,158 +17,160 @@
                     </button>
                     <a href="{{ route('clients') }}" class="waves-light waves-effect btn-flat">Cancel</a>
                     @if(! is_null($client))
-                        <form name="delete-client-form" id="delete-client-form">
+                        <form name="delete-client-form" id="delete-client-form" class="display-inline-block">
                             {!! csrf_field() !!}
-                            <button form="delete-client-form" formaction="{{ route('clients.handleDelete', ['client_id' => $client->getKey()]) }}" formmethod="POST" class="waves-light waves-effect btn red darken-1 margin-left-15">Delete</button>
+                            <button type="submit" form="delete-client-form" formaction="{{ route('clients.handleDelete', ['client_id' => $client->getKey()]) }}" formmethod="POST" class="waves-light waves-effect btn red darken-1 margin-left-15">Delete</button>
                         </form>
                     @endif
                 </div>
             </div>
+            <form id="client-form" name="client-form">
+                {!! csrf_field() !!}
 
-            {{-- Personal Information Card--}}
-            <div class="row">
-                <div class="col s12">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-title">
-                                <span>Personal Information</span>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 m2 input-field">
-                                    <select name="client[title]" id="client-title">
-                                        <option value="" disabled selected>Select</option>
-                                        <option value="Mr">Mr</option>
-                                        <option value="Mrs">Mrs</option>
-                                        <option value="Ms">Ms</option>
-                                        <option value="Sir">Sir</option>
-                                        <option value="Lady">Lady</option>
-                                        <option value="Ind">Ind</option>
-                                    </select>
+                {{-- Personal Information Card--}}
+                <div class="row">
+                    <div class="col s12">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-title">
+                                    <span>Personal Information</span>
                                 </div>
-                                <div class="col s12 m5 input-field">
-                                    <input type="text" class="validate" name="client[first_name]" id="first-name"
-                                           value="@if(! is_null($client)){{ $client->getFirstName() }}@endif" required
-                                           autofocus>
-                                    <label for="first-name">First name</label>
+                                <div class="row">
+                                    <div class="col s12 m2 input-field">
+                                        <select name="client[title]" id="client-title">
+                                            <option value="" disabled selected>Select</option>
+                                            <option value="Mr">Mr</option>
+                                            <option value="Mrs">Mrs</option>
+                                            <option value="Ms">Ms</option>
+                                            <option value="Sir">Sir</option>
+                                            <option value="Lady">Lady</option>
+                                            <option value="Ind">Ind</option>
+                                        </select>
+                                    </div>
+                                    <div class="col s12 m5 input-field">
+                                        <input type="text" class="validate" name="client[first_name]" id="first-name"
+                                               value="@if(! is_null($client)){{ $client->getFirstName() }}@endif" required
+                                               autofocus>
+                                        <label for="first-name">First name</label>
+                                    </div>
+                                    <div class="col s12 m5 input-field">
+                                        <input type="text" class="validate" name="client[last_name]" id="last-name"
+                                               value="@if(! is_null($client)){{ $client->getLastName() }}@endif">
+                                        <label for="last-name">Last name (Optional)</label>
+                                    </div>
                                 </div>
-                                <div class="col s12 m5 input-field">
-                                    <input type="text" class="validate" name="client[last_name]" id="last-name"
-                                           value="@if(! is_null($client)){{ $client->getLastName() }}@endif">
-                                    <label for="last-name">Last name (Optional)</label>
-                                </div>
-                            </div>
 
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[business_name]" id="business-name"
-                                           value="@if(! is_null($client)){{ $client->getBusinessName() }}@endif">
-                                    <label for="business-name">Business name (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[business_name]" id="business-name"
+                                               value="@if(! is_null($client)){{ $client->getBusinessName() }}@endif">
+                                        <label for="business-name">Business name (Optional)</label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            {{-- End personal information --}}
+                {{-- End personal information --}}
 
-            <div class="row">
-                {{-- Address --}}
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-title"><span>Address</span></div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[address_1]" id="street-address"
-                                           value="@if(! is_null($client)){{ $client->getAddress1() }}@endif">
-                                    <label for="street-address">Street Address (Optional)</label>
+                <div class="row">
+                    {{-- Address --}}
+                    <div class="col s12 m6">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-title"><span>Address</span></div>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[address_1]" id="street-address"
+                                               value="@if(! is_null($client)){{ $client->getAddress1() }}@endif">
+                                        <label for="street-address">Street Address (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[address_2]" id="street-address-2"
-                                           value="@if(! is_null($client)){{ $client->getAddress2() }}@endif">
-                                    <label for="street-address-2">Street Address (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[address_2]" id="street-address-2"
+                                               value="@if(! is_null($client)){{ $client->getAddress2() }}@endif">
+                                        <label for="street-address-2">Street Address (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[town_city]" id="city"
-                                           value="@if(! is_null($client)){{ $client->getTownCity() }}@endif">
-                                    <label for="city">City (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[town_city]" id="city"
+                                               value="@if(! is_null($client)){{ $client->getTownCity() }}@endif">
+                                        <label for="city">City (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[postcode]" id="postcode"
-                                           value="@if(! is_null($client)){{ $client->getPostcode() }}@endif">
-                                    <label for="postcode">Postcode (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[postcode]" id="postcode"
+                                               value="@if(! is_null($client)){{ $client->getPostcode() }}@endif">
+                                        <label for="postcode">Postcode (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <select id="country" name="client[country]">
-                                        <option value="" disabled selected>Select your country</option>
-                                        @foreach($countries as $country)
-                                            <option @if(! is_null($client) && $client->getCountry() === $country['alpha3']) selected="selected"
-                                                    @endif value="{{ $country['alpha3'] }}">{{ $country['name'] }}</option>
-                                        @endforeach
-                                    </select>
-                                    <label for="country">Country (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <select id="country" name="client[country]">
+                                            <option value="" disabled selected>Select your country</option>
+                                            @foreach($countries as $country)
+                                                <option @if(! is_null($client) && $client->getCountry() === $country['alpha3']) selected="selected"
+                                                        @endif value="{{ $country['alpha3'] }}">{{ $country['name'] }}</option>
+                                            @endforeach
+                                        </select>
+                                        <label for="country">Country (Optional)</label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                {{-- End Address --}}
+                    {{-- End Address --}}
 
-                {{-- Contact Information --}}
-                <div class="col s12 m6">
-                    <div class="card">
-                        <div class="card-content">
-                            <div class="card-title"><span>Contact Information</span></div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[telephone]" id="telephone"
-                                           value="@if(! is_null($client)){{ $client->getTelephone() }}@endif">
-                                    <label for="telephone">Telephone (Optional)</label>
+                    {{-- Contact Information --}}
+                    <div class="col s12 m6">
+                        <div class="card">
+                            <div class="card-content">
+                                <div class="card-title"><span>Contact Information</span></div>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[telephone]" id="telephone"
+                                               value="@if(! is_null($client)){{ $client->getTelephone() }}@endif">
+                                        <label for="telephone">Telephone (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[fax]" id="fax"
-                                           value="@if(! is_null($client)){{ $client->getFax() }}@endif">
-                                    <label for="fax">Fax (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[fax]" id="fax"
+                                               value="@if(! is_null($client)){{ $client->getFax() }}@endif">
+                                        <label for="fax">Fax (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[mobile]" id="mobile"
-                                           value="@if(! is_null($client)){{ $client->getMobile() }}@endif">
-                                    <label for="mobile">Mobile (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[mobile]" id="mobile"
+                                               value="@if(! is_null($client)){{ $client->getMobile() }}@endif">
+                                        <label for="mobile">Mobile (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[email_address]" id="email-address"
-                                           value="@if(! is_null($client)){{ $client->getEmailAddress() }}@endif">
-                                    <label for="email-address">Email Address (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[email_address]" id="email-address"
+                                               value="@if(! is_null($client)){{ $client->getEmailAddress() }}@endif">
+                                        <label for="email-address">Email Address (Optional)</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="row">
-                                <div class="col s12 input-field">
-                                    <input type="text" class="validate" name="client[web]" id="client-web"
-                                           value="@if(! is_null($client)){{ $client->getWebAddress() }}@endif">
-                                    <label for="client-web">Website (Optional)</label>
+                                <div class="row">
+                                    <div class="col s12 input-field">
+                                        <input type="text" class="validate" name="client[web]" id="client-web"
+                                               value="@if(! is_null($client)){{ $client->getWebAddress() }}@endif">
+                                        <label for="client-web">Website (Optional)</label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
+                    {{-- End contact information --}}
                 </div>
-                {{-- End contact information --}}
-            </div>
 
-            <div class="row">
+                <div class="row">
                 {{-- Additional Information --}}
                 <div class="col s12 m6">
                     <div class="card">
@@ -201,7 +201,7 @@
                 {{-- End additional information --}}
             </div>
 
+            </form>
         </div>
-    </form>
 
 @endsection
