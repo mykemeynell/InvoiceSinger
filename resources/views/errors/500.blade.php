5e4026f96f19e0863a040528d853d33f76928209
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-27 14:40:17 +0100

    added 500 page

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -5,5 +5,15 @@
 @section('message', __('Server Error'))
 
 @section('image')
-    <div style="background-image: url('{{ asset('images/decoration/bg/500-error.jpg') }}');background-position:center;background-size:cover;width:100%;height:100%;"></div>
+    <div style="width:100%;height:100%;position:relative;">
+        <div style="width:60%;position:absolute;top:50%;left:50%;transform:translateY(-50%) translateX(-50%);">
+            <div style="margin:0 auto;">
+                <div style="width:100%;border-bottom:1px solid #d6d6d6;text-align: center;">
+                    <img src="{{ asset('images/decoration/bg/500-terminal.svg') }}" alt="Error 500 terminal" style="width:80%;display:block;margin-left:auto;margin-right:auto;">
+                </div>
+                <img src="{{ asset('images/decoration/bg/terminal-shadow.svg') }}" alt="Shadow">
+            </div>
+        </div>
+
+    </div>
 @endsection
