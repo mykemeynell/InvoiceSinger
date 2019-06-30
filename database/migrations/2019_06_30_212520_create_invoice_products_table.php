9f1a9d945f49f85db45b78c50bab0562008c27b4
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-06-30 22:26:35 +0100

    created migration for invoice products

---
+++ database/migrations/2019_06_30_212520_create_invoice_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use UuidColumn\Concern\UuidColumn;
+
+/**
+ * Class CreateInvoiceProductsTable
+ */
+class CreateInvoiceProductsTable extends Migration
+{
+    use UuidColumn;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('invoice_products', function (Blueprint $table) {
+            $this->createUuidColumn($table, 'id');
+            // TODO: Implement invoice_products table
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('invoice_products');
+    }
+}
