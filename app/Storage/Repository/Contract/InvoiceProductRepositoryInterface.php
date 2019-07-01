15e6e73a2353e367f99a9888e83227661099f7d7
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-01 11:33:52 +0100

    created invoice product repository

---
+++ app/Storage/Repository/Contract/InvoiceProductRepositoryInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace InvoiceSinger\Storage\Repository\Contract;
+
+use ArchLayer\Repository\RepositoryInterface;
+
+/**
+ * Interface InvoiceProductRepositoryInterface.
+ *
+ * @package InvoiceSinger\Storage\Repository\Contract
+ */
+interface InvoiceProductRepositoryInterface extends RepositoryInterface
+{
+}
---
+++ app/Storage/Repository/InvoiceProductRepository.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace InvoiceSinger\Storage\Repository;
+
+use ArchLayer\Repository\Repository;
+use InvoiceSinger\Storage\Repository\Contract\InvoiceProductRepositoryInterface;
+
+/**
+ * Class InvoiceProductRepository.
+ *
+ * @package InvoiceSinger\Storage\Repository
+ */
+class InvoiceProductRepository extends Repository implements InvoiceProductRepositoryInterface
+{
+}
