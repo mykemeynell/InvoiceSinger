fac47f914de6acb83776d2c1654ee35f804f2d42
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-01 11:42:38 +0100

    added Invoice Product Entity to IP Repo

--- app/Storage/Repository/InvoiceProductRepository.php
+++ app/Storage/Repository/InvoiceProductRepository.php
@@ -3,6 +3,7 @@
 namespace InvoiceSinger\Storage\Repository;
 
 use ArchLayer\Repository\Repository;
+use InvoiceSinger\Storage\Entity\Contract\InvoiceProductEntityInterface;
 use InvoiceSinger\Storage\Repository\Contract\InvoiceProductRepositoryInterface;
 
 /**
@@ -12,4 +13,14 @@ use InvoiceSinger\Storage\Repository\Contract\InvoiceProductRepositoryInterface;
  */
 class InvoiceProductRepository extends Repository implements InvoiceProductRepositoryInterface
 {
+    /**
+     * InvoiceProductRepository constructor.
+     *
+     * @param \InvoiceSinger\Storage\Entity\Contract\InvoiceProductEntityInterface|\Illuminate\Database\Eloquent\Model $entity
+     *
+     */
+    function __construct(InvoiceProductEntityInterface $entity)
+    {
+        $this->setModel($entity);
+    }
 }
