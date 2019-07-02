c29dac5a6487ba6915f8f0f121164fef32a426c9
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-02 17:17:46 +0100

    created payment methods table migration

---
+++ database/migrations/2019_07_02_161616_create_payment_methods_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use UuidColumn\Concern\UuidColumn;
+
+/**
+ * Class CreatePaymentMethodsTable.
+ */
+class CreatePaymentMethodsTable extends Migration
+{
+    use UuidColumn;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payment_methods', function (Blueprint $table) {
+            $this->createUuidColumn($table, 'id');
+            $table->string('name');
+            $table->tinyInteger('enabled')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payment_methods');
+    }
+}
