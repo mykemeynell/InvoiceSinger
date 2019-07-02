ee3ba7dfb99c6f88776c82a04d0ff575f46ba9f5
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-03 00:34:21 +0100

    added currency settings

--- app/Http/Controllers/Settings/SettingController.php
+++ app/Http/Controllers/Settings/SettingController.php
@@ -4,6 +4,7 @@ namespace InvoiceSinger\Http\Controllers\Settings;
 
 use Illuminate\View\View;
 use InvoiceSinger\Http\Controllers\Controller;
+use mykemeynell\Support\CurrencyHtmlEntities;
 
 /**
  * Class SettingController
@@ -16,9 +17,13 @@ class SettingController extends Controller
      * Get the settings view.
      *
      * @return \Illuminate\View\View
+     *
+     * @throws \Illuminate\Contracts\Container\BindingResolutionException
      */
     public function index(): View
     {
-        return view('settings');
+        return view('settings')
+            ->with('app_currency', settings('app.currency'))
+            ->with('app_currency_options', app()->make(CurrencyHtmlEntities::class)->all());
     }
 }
--- resources/views/settings/panels/_application.blade.php
+++ resources/views/settings/panels/_application.blade.php
@@ -1,1 +1,23 @@
-Application
+<div class="card">
+    <div class="card-content">
+        <div class="card-title">
+            <div class="row">
+                <div class="col s12">
+                    <span>General Settings</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="row margin-bottom-0">
+            <div class="col s12 input-field">
+                <select name="settings[app.currency]" id="app-currency">
+                    @foreach($app_currency_options as $alpha3 => $entity)
+                        <option value="{{ $alpha3 }}" @if($app_currency == $alpha3) selected="selected" @endif>{{ $alpha3 }} ({!! $entity !!})</option>
+                    @endforeach
+                </select>
+                <label for="app-currency">Currency</label>
+            </div>
+        </div>
+
+    </div>
+</div>
