b92338d20e97f40f61f40d836e91052e573520dc
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-02 17:14:58 +0100

    created migration for payments table

---
+++ database/migrations/2019_07_02_161310_create_payments_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use UuidColumn\Concern\UuidColumn;
+
+/**
+ * Class CreatePaymentsTable.
+ */
+class CreatePaymentsTable extends Migration
+{
+    use UuidColumn;
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $this->createUuidColumn($table, 'id');
+            $table->string('invoice');
+            $table->float('amount', 10, 2);
+            $table->date('paid_at');
+            $table->text('notes')->nullable();
+            $table->text('payload')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
