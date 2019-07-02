4f0322d85a65965e8354d6bde8e0c132f4b68445
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-03 00:47:59 +0100

    WIP - settings

--- resources/views/settings/panels/_invoices.blade.php
+++ resources/views/settings/panels/_invoices.blade.php
@@ -1,1 +1,22 @@
-Invoices
+<div class="card">
+    <div class="card-content">
+        <div class="row">
+            <div class="col s12 input-field">
+                <input type="text" id="invoice-pattern" name="settings[invoice.pattern]" value="{{ settings('invoice.pattern') }}">
+                <label for="invoice-pattern">Invoice Pattern</label>
+            </div>
+        </div>
+
+        <div class="row margin-bottom-0">
+            <div class="col s12 input-field">
+                <select name="settings[invoice.term]" id="invoice-term">
+                    <option value="10 days" @if(settings('invoice.term') == '10 days') selected="selected" @endif>NET 10</option>
+                    <option value="15 days" @if(settings('invoice.term') == '15 days') selected="selected" @endif>NET 15</option>
+                    <option value="30 days" @if(settings('invoice.term') == '30 days') selected="selected" @endif>NET 30</option>
+                    <option value="60 days" @if(settings('invoice.term') == '60 days') selected="selected" @endif>NET 60</option>
+                </select>
+                <label for="invoice-pattern">Invoice Term (Net D)</label>
+            </div>
+        </div>
+    </div>
+</div>
