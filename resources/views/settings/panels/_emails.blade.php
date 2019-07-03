e24406eb00426e8e2a5c8db553ee4f5f48113660
Myke Meynell <1590190+mykemeynell@users.noreply.github.com>
2019-07-03 23:07:57 +0100

    changed email settings to match those in laravel - override

--- resources/views/settings/panels/_emails.blade.php
+++ resources/views/settings/panels/_emails.blade.php
@@ -3,7 +3,7 @@
         <div class="row margin-bottom-30">
             <div class="col s12 input-field">
                 <label>
-                    <input type="checkbox" name="settings[email.attach]" class="filled-in"  @if(settings('app.online_payments.enabled') == true) checked="checked" @endif>
+                    <input type="checkbox" name="settings[mail.attach]" class="filled-in"  @if(settings('app.online_payments.enabled') == true) checked="checked" @endif>
                     <span>Attach Quote/Invoice to email</span>
                 </label>
             </div>
@@ -11,11 +11,11 @@
 
         <div class="row padding-top-30">
             <div class="col s12 input-field">
-                <select name="settings[email.provider]" id="email-provider">
-                    <option value="none" @if(settings('email.provider') == 'none') selected="selected" @endif>None</option>
-                    <option value="phpmail" @if(settings('email.provider') == 'phpmail') selected="selected" @endif>PHP Mail</option>
-                    <option value="sendmail" @if(settings('email.provider') == 'sendmail') selected="selected" @endif>Sendmail</option>
-                    <option value="smtp" @if(settings('email.provider') == 'smtp') selected="selected" @endif>SMTP</option>
+                <select name="settings[mail.provider]" id="email-provider">
+                    <option value="none" @if(settings('mail.provider') == 'none') selected="selected" @endif>None</option>
+                    <option value="phpmail" @if(settings('mail.provider') == 'phpmail') selected="selected" @endif>PHP Mail</option>
+                    <option value="sendmail" @if(settings('mail.provider') == 'sendmail') selected="selected" @endif>Sendmail</option>
+                    <option value="smtp" @if(settings('mail.provider') == 'smtp') selected="selected" @endif>SMTP</option>
                 </select>
                 <label for="email-provider">Email Provider</label>
             </div>
@@ -24,30 +24,30 @@
         <div id="email-options">
             <div class="row padding-top-15">
                 <div class="col s6 input-field">
-                    <input type="text" name="settings[email.from_address]" value="{{ settings('email.from_address') }}" id="email-from_address">
+                    <input type="text" name="settings[mail.from_address]" value="{{ settings('mail.from_address') }}" id="email-from_address">
                     <label for="email-from_address">From Address</label>
                 </div>
                 <div class="col s6 input-field">
-                    <input type="text" name="settings[email.from_name]" value="{{ settings('email.from_name') }}" id="email-from_name">
+                    <input type="text" name="settings[mail.from_name]" value="{{ settings('mail.from_name') }}" id="email-from_name">
                     <label for="email-from_name">From Name</label>
                 </div>
             </div>
             <div class="row padding-top-15">
                 <div class="col s6 input-field">
-                    <input type="text" name="settings[email.username]" value="{{ settings('email.username') }}" id="email-username">
+                    <input type="text" name="settings[mail.username]" value="{{ settings('mail.username') }}" id="email-username">
                     <label for="email-username">Username</label>
                 </div>
                 <div class="col s6 input-field">
-                    <input type="password" name="settings[email.password]" id="email-password">
+                    <input type="password" name="settings[mail.password]" id="email-password">
                     <label for="email-password">Password</label>
                 </div>
             </div>
             <div class="row padding-top-15">
                 <div class="col s6 input-field">
-                    <select type="text" name="settings[email.encryption]" value="{{ settings('email.encryption') }}" id="email-encryption">
-                        <option value="none" @if(settings('email.encryption') == 'none') selected @endif>None</option>
-                        <option value="tls" @if(settings('email.encryption') == 'tls') selected @endif>TLS</option>
-                        <option value="ssl" @if(settings('email.encryption') == 'ssl') selected @endif>SSL</option>
+                    <select type="text" name="settings[mail.encryption]" value="{{ settings('mail.encryption') }}" id="email-encryption">
+                        <option value="none" @if(settings('mail.encryption') == 'none') selected @endif>None</option>
+                        <option value="tls" @if(settings('mail.encryption') == 'tls') selected @endif>TLS</option>
+                        <option value="ssl" @if(settings('mail.encryption') == 'ssl') selected @endif>SSL</option>
                     </select>
                     <label for="email-encryption">Encryption</label>
                 </div>
